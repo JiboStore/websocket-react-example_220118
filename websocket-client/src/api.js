6944012c9006a2247f798017303be8cd5b41d02e
Hakim Hauston <hakim.hauston@storms.com>
2022-01-18 13:51:30 +0800

    fix .env variable
    -. https://stackoverflow.com/questions/48378337/create-react-app-not-picking-up-env-files

--- websocket-client/src/api.js
+++ websocket-client/src/api.js
@@ -1,6 +1,9 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8000');
+const url = process.env.REACT_APP_PUBLIC_URL;
+console.log(url);
+// const socket = openSocket('http://localhost:8000');
+const socket = openSocket(url);
 
 function subscribeToTimer(callback) {
   socket.on('timer', timestamp => callback(null, timestamp));
--- websocket-client/src/registerServiceWorker.js
+++ websocket-client/src/registerServiceWorker.js
@@ -19,18 +19,23 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
+  console.log('register publicUrl: ' + process.env.REACT_APP_PUBLIC_URL);
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-      return;
-    }
+    const publicUrl = new URL(process.env.REACT_APP_PUBLIC_URL, window.location);
+    // if (publicUrl.origin !== window.location.origin) {
+    //   // Our service worker won't work if PUBLIC_URL is on a different origin
+    //   // from what our page is served on. This might happen if a CDN is used to
+    //   // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+    //   return;
+    // }
+
+    console.log('publicUrl: ', publicUrl);
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.REACT_APP_PUBLIC_URL}/service-worker.js`;
+
+      console.log('swUrl: ' + swUrl);
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
